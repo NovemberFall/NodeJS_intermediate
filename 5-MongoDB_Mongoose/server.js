38cc65f39675924eb02709169feed952adbdcf95
Git <1219.zheng@gmail.com>
2019-12-20 13:38:10 -0800

    update

---
+++ 5-MongoDB_Mongoose/server.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const app = require('./app2');
+
+//envrionment variables is global variables
+// console.log(app.get('env'));
+
+// console.log(process.env);
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+    console.log(`${__dirname}`)
+});
